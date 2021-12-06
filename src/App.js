06e9152f566cbb246718e684b1e50816ef7428d7
Nguyen Cong Duy <ncongduy@gmail.com>
2021-12-06 23:16:30 +0200

    lesson: useEffect - cretate pagination

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
+import Pagination from './components/Pagination';
 import PostList from './components/PostList';
 
 function App() {
 	const [postList, setPostList] = useState([]);
+	const [pagination, setPagination] = useState({
+		_page: 1,
+		_limit: 10,
+		_totalRows: 1,
+	});
+	const [filters, setFilters] = useState({
+		_limit: 10,
+		_page: 1,
+	});
 
 	useEffect(() => {
 		try {
 			async function fetchData() {
-				const requestUrl =
-					'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+				const requestUrl = `http://js-post-api.herokuapp.com/api/posts?_limit=${filters._limit}&_page=${filters._page}`;
 				const response = await fetch(requestUrl);
 				const responseJson = await response.json();
-				const { data } = responseJson;
+				const { data, pagination } = responseJson;
 
 				setPostList(data);
+				setPagination(pagination);
 			}
 
 			fetchData();
 		} catch (error) {
 			console.log('Error when get data from API', error.messeage);
 		}
-	}, []);
+	}, [filters]);
+
+	function handlePageChange(newPage) {
+		setFilters({
+			...filters,
+			_page: newPage,
+		});
+	}
 
 	return (
 		<div className='app'>
 			<h1>React Hooks - Todo List</h1>
 			<PostList postList={postList} />
+			<Pagination
+				pagination={pagination}
+				onPageChange={handlePageChange}
+			/>
 		</div>
 	);
 }
---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+Pagination.propTypes = {
+	pagination: PropTypes.object.isRequired,
+	onPageChange: PropTypes.func,
+};
+
+Pagination.defaultProps = {
+	onPageChange: null,
+};
+
+function Pagination(props) {
+	const { pagination, onPageChange } = props;
+	const { _page, _limit, _totalRows } = pagination;
+	const maxPage = Math.ceil(_totalRows / _limit);
+
+	function handleClick(newPage) {
+		if (!onPageChange) return;
+		onPageChange(newPage);
+	}
+
+	return (
+		<div class='pagination'>
+			<button
+				disabled={_page <= 1}
+				onClick={() => handleClick(_page - 1)}
+			>
+				Prev
+			</button>
+
+			<span>{_page}</span>
+
+			<button
+				disabled={_page >= maxPage}
+				onClick={() => handleClick(_page + 1)}
+			>
+				Next
+			</button>
+		</div>
+	);
+}
+
+export default Pagination;
